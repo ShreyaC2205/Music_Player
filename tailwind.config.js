ceb7fdd06a3b14bfac29aa523668bbc488f70281
Shreya chincholkar <chincholkarshreya@gmail.com>
2024-08-25 08:57:02 +0530

    Few changes done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./index.html",
+    "./script.js",
+    "./style.css",
+    // Add paths to other files where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
